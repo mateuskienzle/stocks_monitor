6782e2d0ba8634473ed0e00cbc487a216669ab73
RodrigoVanzelotti <rodrigovanzelotti@gmail.com>
2023-01-24 15:58:33 -0300

    criado graph 1

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 import dash
 import dash_bootstrap_components as dbc
+from dash_bootstrap_templates import load_figure_template
 
 estilos = ["https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css", "https://fonts.googleapis.com/icon?family=Material+Icons"]
 dbc_css = "https://cdn.jsdelivr.net/gh/AnnMarieW/dash-bootstrap-templates@V1.0.4/dbc.min.css"
 # FONT_AWESOME = "https://use.fontawesome.com/releases/v5.10.2/css/all.css"
+
+load_figure_template("solar")
 
 
 app = dash.Dash(__name__, external_stylesheets=estilos + [dbc_css] + [dbc.themes.SOLAR])
--- components/page_inicial.py
+++ components/page_inicial.py
@@ -5,8 +5,27 @@ import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
 from app import *
+from datetime import datetime, date
 
-height_style={'height': '100%'}
+import yfinance as yf
+
+HEIGHT={'height': '100%'}
+PERIOD_OPTIONS = ['5d','1mo','3mo','6mo','1y','2y','5y','10y','ytd']
+MAIN_CONFIG = {
+    "hovermode": "x unified",
+    "legend": {"yanchor":"top", 
+                "y":0.9, 
+                "xanchor":"left",
+                "x":0.1,
+                "title": {"text": None},
+                "font" :{"color":"white"},
+                "bgcolor": "rgba(0,0,0,0.5)"},
+    "margin": {"l":0, "r":0, "t":10, "b":0}
+}
+
+# Salvar esse df_carteira em um dcc.Store(id=' ', data={}) -> df_carteira.to_dict()
+fake_data = [['PETR4.SA', 23, 323, datetime(2017, 1, 1).date()], ['ITUB4.SA', 23, 323, datetime(2017, 1, 1).date()], ['TTEN3.SA', 23, 323, datetime(2017, 1, 1).date()]]
+df_carteira = pd.DataFrame(columns=['nome', 'preço', 'quantidade', 'data'])
 
 # =========  Layout  =========== #
 layout = dbc.Container([
@@ -16,10 +35,27 @@ layout = dbc.Container([
         dbc.Col([
             dbc.Card([
                 dbc.CardBody([
-                    dcc.Dropdown(id='dropdown_card1', className='dbc', multi=True, value='AAPL', options=[{"label": 'MAT', "value": 'MAT'}, {"label": 'AAPL', "value": 'AAPL'}]),
-                    dcc.Graph(id='line_graph', className='dbc', config={"displayModeBar": False, "showTips": False})
+                    dbc.Row([
+                        dbc.Col([
+                            dcc.Dropdown(id='dropdown_card1', className='dbc', value='PETR4.SA', options=[{"label": 'PETR4.SA', "value": 'PETR4.SA'}, {"label": 'ITUB4.SA', "value": 'ITUB4.SA'}, {"label": "TTEN3.SA", "value": "TTEN3.SA"}]),
+                        ], sm=12, md=3),
+                        dbc.Col([
+                            dbc.RadioItems(
+                                options=[{'label': x, 'value': x} for x in PERIOD_OPTIONS],
+                                value='3mo',
+                                id="period_input",
+                                inline=True
+                            ),
+                        ], sm=12, md=9)
+                    ]),
+                    dbc.Row([
+                        dbc.Col([
+                            dcc.Graph(id='line_graph', config={"displayModeBar": False, "showTips": False})    
+                        ])
+                    ])
+                    
                 ])
-            ], style=height_style)
+            ], style=HEIGHT)
         ], xs=12, md=8),
         # Card 2
         dbc.Col([
@@ -27,9 +63,9 @@ layout = dbc.Container([
                 dbc.CardBody([
                     dbc.Checklist(id='checklist_card2', value=[1], inline=True,
                         options=[{'label': 'moda', 'value': 1}, {'label': 'siderurgica', 'value': 2}],persistence=True, persistence_type="session"),
-                    dcc.Graph(id='radar_graph', className='dbc', config={"displayModeBar": False, "showTips": False})
+                    dcc.Graph(id='radar_graph', config={"displayModeBar": False, "showTips": False})
                 ])
-            ], style=height_style)
+            ], style=HEIGHT)
         ], xs=12, md=4)
     ], className='g-2 my-auto'),
     # Linha 2
@@ -40,7 +76,7 @@ layout = dbc.Container([
                 dbc.Col([
                     dbc.Card([
                         dbc.CardBody([
-                            dcc.Graph(id='indicator_graph', className='dbc', config={"displayModeBar": False, "showTips": False})
+                            dcc.Graph(id='indicator_graph', config={"displayModeBar": False, "showTips": False})
                         ])
                     ], style={'height': '100%'})
                 ], xs=6, md=4),
@@ -49,22 +85,22 @@ layout = dbc.Container([
                         dbc.Col([
                             dbc.Card([
                                 dbc.CardBody([
-                                    dcc.Graph(id='line2_graph', className='dbc', config={"displayModeBar": False, "showTips": False})
+                                    dcc.Graph(id='line2_graph', config={"displayModeBar": False, "showTips": False})
                                 ])
-                            ], style=height_style)
+                            ], style=HEIGHT)
                         ])
                     ], className='my-auto', style={'height': '50%'}),
                     dbc.Row([
                         dbc.Col([
                             dbc.Card([
                                 dbc.CardBody([
-                                    dcc.Graph(id='podium_graph', className='dbc', config={"displayModeBar": False, "showTips": False})
+                                    dcc.Graph(id='podium_graph', config={"displayModeBar": False, "showTips": False})
                                 ])
-                            ], style=height_style)
+                            ], style=HEIGHT)
                         ])
                     ], className='g-2 my-auto', style={'height': '50%'})
                 ], xs=6, md=8)
-            ], style=height_style, className='g-2')
+            ], style=HEIGHT, className='g-2')
         ], xs=12, md=6),
         # Card 4
         dbc.Col([
@@ -83,11 +119,32 @@ layout = dbc.Container([
 # callback card 1
 @app.callback(
     Output('line_graph', 'figure'),
-    Input('dropdown_card1', 'value')
+    Input('dropdown_card1', 'value'),
+    Input('period_input', 'value')
 )
-def func_card1(dropdown):
-    print(dropdown)
-    return {}
+def func_card1(dropdown, period):
+    bov = yf.Ticker('^BVSP')
+    ticker = yf.Ticker(dropdown)
+    data_ibovespa = bov.history(period=period, interval='1d')['Close']
+    data_ticker = ticker.history(period=period, interval='1d')['Close']
+
+    data_ibovespa = data_ibovespa.pct_change()*100
+    data_ticker = data_ticker.pct_change()*100
+
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x=data_ibovespa.index, y=data_ibovespa.values, name='BVSP', mode='lines', line_shape='spline'))
+    fig.add_trace(go.Scatter(x=data_ticker.index, y=data_ticker.values, name=dropdown, mode='lines', line_shape='spline'))
+
+    fig.update_layout(MAIN_CONFIG, height=250, yaxis={'ticksuffix': '%'})
+    fig.add_annotation(text=f'Variação em % - BVSP x {dropdown}',
+        xref="paper", yref="paper",
+        font=dict(
+            family="Courier New, monospace",
+            size=20,
+            color="#ffffff"),
+        align="center", bgcolor="rgba(0,0,0,0.5)", opacity=0.8,
+        x=0.9, y=0.1, showarrow=False)
+    return fig
 
 # callback card 2
 @app.callback(
@@ -96,4 +153,29 @@ def func_card1(dropdown):
 )
 def func_card2(checklist):
     print(checklist)
+    '''
+    Pegar todos os ticks e a sua representação no df. Na sequencia, ponderar essas representações a partir dos setores utilzando o código que foi comentado aqui abaixo
+    '''
+    data = []
+    quant = 200
+
+    # for tick in checklist:
+    #     if tick.info == None: continue
+    #     data.append({'Setor': tick.info['sector'], 'Quantidade': quant})
+    #     quant += quant*1/4
+    # df = pd.DataFrame(data)
+
+    # fig = go.Figure()
+    # fig.add_trace(go.Scatterpolar(r=df['Quantidade']/100, theta=df['Setor'], fill='toself', name='Minha Carteira'))
+    # fig.update_layout(height=500)
+    # fig.add_annotation(text=f'Distribuição da carteira relativo à BVSP/setor',
+    #     xref="paper", yref="paper",
+    #     font=dict(
+    #         family="Courier New, monospace",
+    #         size=20,
+    #         color="#ffffff"),
+    #     align="center", bgcolor="rgba(0,0,0,0.5)", opacity=0.8,
+    #     x=0.9, y=0.1, showarrow=False)
+
+
     return {}
