54fa4562ce2ea724f717443be68ddb7ec8a4d22e
RodrigoVanzelotti <rodrigovanzelotti@gmail.com>
2023-01-29 22:38:49 -0300

    adicionado a lista de cards

--- components/wallet.py
+++ components/wallet.py
@@ -11,88 +11,77 @@ from app import *
 
 lista_de_ativos = [{"Data" : "25/01/2023", "Nome" : "ITUB4", "Quantidade" : "10", "Valor unitário" : "R$9,47", "Valor total" : " R$94,70", "id" : 0}]
 
-
-new_card =  dbc.Row([
-
-        dbc.Col([
-
-            dbc.Card([
-
-                dbc.CardBody([
-
-                    dbc.Row([
-
-                        dbc.Col([
-
-                            dbc.Row([
-
-                                dbc.Col([
-                                    html.Legend("Nome: " + lista_de_ativos[0]['Nome'])
+def generate_card(info_do_ativo):
+    new_card =  dbc.Row([
+            dbc.Col([
+                dbc.Card([
+                    dbc.CardBody([
+                        dbc.Row([
+                            dbc.Col([
+                                dbc.Row([
+                                    dbc.Col([
+                                        html.Legend("Nome: " + info_do_ativo['ativo'])
+                                    ]),
                                 ]),
-                            ]),
+                                dbc.Row([
 
-                            dbc.Row([
-
-                                dbc.Col([
-                                    html.Legend("Quantidade: " + lista_de_ativos[0]['Quantidade'])
+                                    dbc.Col([
+                                        html.Legend("Quantidade: " + str(info_do_ativo['vol']))
+                                    ]),
                                 ]),
-                            ]),
-
-                            dbc.Row([
-                                dbc.Col([
-                                    html.Legend("Valor unitário: " + lista_de_ativos[0]['Valor unitário'])
+                                dbc.Row([
+                                    dbc.Col([
+                                        html.Legend("Valor unitário: " + str(info_do_ativo['preco']))
+                                    ]),
                                 ]),
-                            ]),
-
-                            dbc.Row([
-
-                                dbc.Col([
-                                    html.Legend("Data: " + lista_de_ativos[0]['Data'])
+                                dbc.Row([
+                                    dbc.Col([
+                                        html.Legend("Data: " + info_do_ativo['date'].strftime('%Y-%m-%d'))
+                                    ]),
                                 ]),
-
-                            ]),
-
-                        ], md=6, xs=6),
-
-                        dbc.Col([
-                            dbc.Row([
-                                dbc.Col([
-                                    html.Img(src="https://petrobras.com.br/sitepetrobras/imgs/bg/logo-social.png", style={'width' : '10%', 'margin-top' : '1rem', 'border-radius' : '15%'}),
-                                    html.Legend("Valor total: " + lista_de_ativos[0]['Valor total'])
+                            ], md=6, xs=6),
+                            dbc.Col([
+                                dbc.Row([
+                                    dbc.Col([
+                                        html.Img(src=info_do_ativo['logo_url'], style={'width' : '10%', 'margin-top' : '1rem', 'border-radius' : '15%'}),
+                                        html.Legend("Valor total: " + info_do_ativo['preco']*info_do_ativo['vol'])
+                                    ]),
                                 ]),
-                            ]),
-                        ], md=5, xs=6, style={'text-align' : 'right'}),
-
-                        dbc.Col([
-                            dbc.Button([html.I(className = "fa fa-trash header-icon", 
-                                                style={'font-size' : '200%'})],
-                                                id={'type': 'delete_event','index': lista_de_ativos[0]['id']},
-                                                style={'background-color' : 'transparent', 'border-color' : 'transparent'}
-                                            ), 
-                        ], md=1, xs=12, style={'text-align' : 'right'})
+                            ], md=5, xs=6, style={'text-align' : 'right'}),
+                            dbc.Col([
+                                dbc.Button([html.I(className = "fa fa-trash header-icon", 
+                                                    style={'font-size' : '200%'})],
+                                                    id={'type': 'delete_event','index': lista_de_ativos['id']},
+                                                    style={'background-color' : 'transparent', 'border-color' : 'transparent'}
+                                                ), 
+                            ], md=1, xs=12, style={'text-align' : 'right'})
+                        ])
                     ])
                 ])
             ])
-        ])
-    ], className='g-2 my-auto')
+        ], className='g-2 my-auto')
+
+    return new_card
 
 
-layout = dbc.Container([
-
-  new_card,
-  new_card,
-  new_card,
-  new_card,
-  new_card
-
-], id= 'layout_wallet', fluid=True)
+layout = dbc.Container(children=[], id= 'layout_wallet', fluid=True)
 
 @app.callback(
-    Output ('layout_wallet' 'children'),
-    Input  ('book_data_store' , 'data'),
-    
-
+    Output ('layout_wallet', 'children'),
+    Input  ('book_data_store', 'data'),
 )
 
-def update_wallet(children):
-    pass
+def update_wallet(data):
+    df = pd.DataFrame(data)
+
+    lista_de_dicts = []
+    for row in df.index:
+        infos = df.loc[row].to_dict()
+        lista_de_dicts.append(infos)
+
+    lista_de_cards = []
+    for dicio in lista_de_dicts:
+        card = generate_card(dicio)
+        lista_de_cards.append(card)
+    
+    return lista_de_cards
